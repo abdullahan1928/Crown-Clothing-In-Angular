b9d80092c1b7e546659ad3731ca22de09bd21e8b
abdullahan1928 <abdullahan1928.com>
2022-11-22 19:51:09 +0500

    Added env prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'crown-clothing-f9d06',
+    appId: '1:526573944941:web:a6177f88a86c2ba42abf64',
+    storageBucket: 'crown-clothing-f9d06.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDt5vNFOxulBdPtd9ma1z2a4cLMwsnHCvw',
+    authDomain: 'crown-clothing-f9d06.firebaseapp.com',
+    messagingSenderId: '526573944941',
+  },
+  production: true
+};
